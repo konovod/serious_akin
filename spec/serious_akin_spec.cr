2fc3766bf58682a8afd256889bf428f4c9352174
Konovod <konovod@gmail.com>
2017-06-06 21:15:38 +0000

    first spec - adding fact

--- spec/serious_akin_spec.cr
+++ spec/serious_akin_spec.cr
@@ -1,9 +1,16 @@
 require "./spec_helper"
 
+include SeriousAkin
 describe SeriousAkin do
   # TODO: Write tests
 
-  it "works" do
-    false.should eq(true)
+  db = Database.new
+
+  it "db add records" do
+    db.add_record "кот", "длинный хвост"
+    db.all_questions.to_a.should eq ["длинный хвост"]
+    db.all_items.to_a.should eq ["кот"]
+    db.data.size.should eq 1
+    db.data[{"кот", "длинный хвост"}].should eq Answer::Yes
   end
 end
---
+++ src/serious_akin/trivial.cr
@@ -0,0 +1,69 @@
+module SeriousAkin
+  enum Answer
+    Unknown
+    Yes
+    No
+    Incorrect
+  end
+
+  enum RoundStage
+    Question
+    Guess
+    Won
+    Lost
+  end
+
+  alias Item = String
+  alias Question = String
+
+  alias History = Hash(Question, Answer)
+  alias ItemSet = Set(Item)
+  alias Stats = Hash(Answer, Int32)
+
+  class Round
+    @history = History.new
+    @set = ItemSet.new
+    getter stage = RoundStage::Question
+    @db : Database
+
+    def initialize(@db)
+    end
+
+    def next_question(output : IO)
+    end
+
+    def process_input(input : IO)
+    end
+  end
+
+  class Database
+    getter data = Hash({Item, Question}, Answer).new(Answer::Unknown)
+    getter all_questions = Set(Question).new
+    getter all_items = Set(Item).new
+
+    def add_record(item, question, history = History.new)
+      @all_items << item
+      @all_questions << question
+      @data[{item, question}] = Answer::Yes
+      history.each do |q, ans|
+        @data[{item, q}] = ans
+      end
+    end
+
+    def update_record(item, history)
+      history.each do |q, ans|
+        @data[{item, q}] = ans
+      end
+    end
+
+    def best_question(set, history)
+      @all_questions.select { |q| history[q]? }.max_by do |q|
+        st = Stats.new(0)
+        @data.each do |(item, q1), ans|
+          st[ans] += 1 if q1 == q && set.contain?(item)
+        end
+        {st[Answer::Yes], st[Answer::No]}.min
+      end
+    end
+  end
+end
