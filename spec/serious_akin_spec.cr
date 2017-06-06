a9b59686806d4b79d90c24d511d08ff955d92750
Konovod <konovod@gmail.com>
2017-06-06 21:19:20 +0000

    adding second item spec

--- spec/serious_akin_spec.cr
+++ spec/serious_akin_spec.cr
@@ -13,4 +13,14 @@ describe SeriousAkin do
     db.data.size.should eq 1
     db.data[{"кот", "длинный хвост"}].should eq Answer::Yes
   end
+  it "db add more records" do
+    db.add_record "собака", "лает", {"длинный хвост" => Answer::Yes}
+    db.all_questions.should contain "лает"
+    db.all_items.should contain "собака"
+    db.data.size.should eq 3
+    db.data[{"собака", "длинный хвост"}].should eq Answer::Yes
+    db.data[{"собака", "лает"}].should eq Answer::Yes
+    db.data[{"кот", "лает"}].should eq Answer::Unknown
+    db.data[{"кот", "длинный хвост"}].should eq Answer::Yes
+  end
 end
