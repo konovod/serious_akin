17c95f710a830bb58f9c6b26e9b32829421d3236
Konovod <konovod@gmail.com>
2017-06-06 22:16:54 +0000

    dialog works

--- src/dialog.cr
+++ src/dialog.cr
@@ -1,16 +1,18 @@
 require "./serious_akin"
 
+include SeriousAkin
+
 def input_str(msg)
   puts "#{msg}?"
-  gets.chomp
+  gets.not_nil!.chomp
 end
 
 def input_ans(msg) : SeriousAkin::Answer
   s = input_str "#{msg}(Д - Дa/Н - Нет/З - Затрудняюсь)"
-  case s.locase[0]
-  when "д", "y"
+  case s.downcase[0]
+  when 'д', 'y'
     Answer::Yes
-  when "н", "n"
+  when 'н', 'n'
     Answer::No
   else
     Answer::Incorrect
@@ -22,11 +24,22 @@ db.add_record "кот", "длинный хвост"
 
 loop do
   puts "-----------НОВЫЙ РАУНД----------"
-  i = 1
   set = db.all_items.to_set
-  while !set.size > 1
-    question = db.best_question(set)
+  history = History.new
+  while set.size > 1
+    question = db.best_question(set, history)
     ans = input_ans(question)
-    set = db.partition(question)
+    db.partition(set, question, ans)
+    history[question] = ans
+  end
+  guess = set.first.not_nil!
+  case input_ans "Это #{guess}! Я угадал"
+  when .no?
+    what = input_str("Тогда кто это")
+    diff = input_str("Чем отличается от #{guess}")
+    db.add_record what, diff, history
+    db.update_record guess, {diff => Answer::No}
+  else
+    puts "УРА"
   end
 end
